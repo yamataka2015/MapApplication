ecf2338e3020bab6ca58fdfc882481b86d9127d3
nakamuramadoka <sakura6ren20@gmail.com>
2015-10-27 17:36:58 +0900

    nakamura

--- app/src/main/java/com/example/MapApplication/MainActivity.java
+++ app/src/main/java/com/example/MapApplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); //nakamura comment
 
 		// Move to UserRegistActivity when first run
 		InputStream in = null;
