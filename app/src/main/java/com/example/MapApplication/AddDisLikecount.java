5d47816788fdc5a0d099a178ac66d3230a2579f2
nakamuramadoka <sakura6ren20@gmail.com>
2015-11-24 17:30:44 +0900

    nakamura

---
+++ app/src/main/java/com/example/MapApplication/AddDisLikecount.java
@@ -0,0 +1,7 @@
+package com.example.MapApplication;
+
+/**
+ * Created by MADOKA on 2015/11/17.
+ */
+public class AddDisLikecount {
+}
---
+++ app/src/main/java/com/example/MapApplication/DeletePlace.java
@@ -0,0 +1,7 @@
+package com.example.MapApplication;
+
+/**
+ * Created by MADOKA on 2015/11/19.
+ */
+public class DeletePlace {
+}
